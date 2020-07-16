f493ae198e087470a5bcdb836ef056ad874e1bb0
LavaPower <lavapower84@gmail.com>
2020-07-16 16:36:14 +0200

    Update Test

--- src/test/java/fr/lavapower/exalt/ExaltTest.java
+++ src/test/java/fr/lavapower/exalt/ExaltTest.java
@@ -1,10 +1,8 @@
 package fr.lavapower.exalt;
 
-import fr.lavapower.exalt.collision.CollisionInfo;
 import fr.lavapower.exalt.component.*;
 import fr.lavapower.exalt.entity.Entity;
 import fr.lavapower.exalt.exceptions.IllegalComponentException;
-import fr.lavapower.exalt.render.Font;
 import junit.framework.*;
 
 public class ExaltTest extends TestCase
@@ -12,28 +10,31 @@ public class ExaltTest extends TestCase
     private static Exalt exalt;
     private static boolean setUpIsDone = false;
 
-    public void collision(CollisionInfo collisionInfo) {
-        collisionInfo.getOtherEntity().kill();
-    }
-
     protected void setUp() throws Exception {
         if(setUpIsDone) return;
 
-        exalt = new Exalt();
-        exalt.setDebug(true);
-        exalt.center();
+        exalt = new Exalt().debug(true).center();
+
+        System.out.println(exalt.getClipboard());
+        exalt.setClipboard("TESTING");
 
         Entity e = new Entity();
         e.addComponents(new PositionComponent(0, 0), new SpriteComponent("resources/test.png"), new ControlComponent(), new CollisionComponent());
 
         Entity e2 = new Entity();
         e2.addComponents(new PositionComponent(100, 0), new SpriteComponent("resources/test.png"),
-                new CollisionComponent(collisionInfo -> collisionInfo.getMyEntity().kill()), new AutoComponent(-2, 0, 10));
+                new CollisionComponent(collisionInfo -> {
+                    try
+                    {
+                        collisionInfo.getMyEntity().delete();
+                    }
+                    catch(IllegalComponentException illegalComponentException)
+                    {
+                        illegalComponentException.printStackTrace();
+                    }
+                }), new AutoComponent(-2, 0, 10));
 
-        Entity e3 = new Entity();
-        e3.addComponents(new PositionComponent(-100, -100), new TextComponent(new Font()));
-
-        exalt.getWorld().getEntitySystem().addEntities(e, e2, e3);
+        exalt.getWorld().getEntitySystem().addEntities(e, e2);
 
         setUpIsDone = true;
     }
